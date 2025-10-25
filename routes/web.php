43bf74d04742c1d7c9bf33886202147e2c84a327
Waghlalit <waghlalit6543@gmail.com>
2025-10-26 02:20:11 +0530

    first c

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
+
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
